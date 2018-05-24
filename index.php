a2948397b0f31dc4893c9db91fb50770e5d0114d
Gustavo Katcheroff <gustavo.katcheroff@tangomodem.com>
2018-05-24 15:04:57 -0300

    First release

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
+if (isset($_GET["img"])) {
 
-require 'vendor/autoload.php';
-
-function ServiceHandler() {
-    $data = new stdClass();
-    $data->Output = "Hello World!";
-
-    return json_encode($data);
-};
-
-$app = new \Slim\App;
-
-$app->get('/', 'ServiceHandler');
-$app->post('/', 'ServiceHandler');
-
-$app->run();
+	$url = $_GET["img"];
+	$file = basename($url);
+	
+	// check whitelist
+	
+	$parse = parse_url($url);
+	$host = "";
+	if (isset($parse['host'])) {
+		$host = $parse['host'];
+	}
+	
+	switch ($host) {
+		case "horizonlives3.s3.eu-west-1.amazonaws.com":
+		case "horizonlives3.s3-eu-west-1.amazonaws.com":
+		case "duijuz32qudrm.cloudfront.net":
+			header("Content-Disposition: attachment; filename=\"". $file ."\";" );
+			echo file_get_contents($url);
+			break;
+		default:
+			header('HTTP/1.1 403 Forbidden');
+	};
+} else {
+	header("HTTP/1.0 404 Not Found");
+}
+?>
